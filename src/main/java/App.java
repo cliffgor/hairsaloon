62f4dc0e09cefe3ff5a7b4cbf73f3910d1f0903a
cliffgor <clierozigy@gmail.com>
2017-08-31 09:33:23 +0300

    added more functionality on app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Map;
 
 import java.util.HashMap;
@@ -11,13 +12,51 @@ import static spark.Spark.*;
 
 public class App{
 
-    public static void main(String[] args){
+    private static ArrayList<Hair> hair;
+
+	public static void main(String[] args){
 
         staticFileLocation("/public");
 
-        String layout ="tepmplates/layout.vtl";
+        String layout ="templates/layout.vtl";
 
-        
+        // a route that renders the home page
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("hairs", request.session().attribute("hairs"));
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+        post("/hairs", (request, response) -> {
+            
+                Map<String, Object> model = new HashMap<String, Object>();
+            
+                ArrayList<Hair> hairs = request.session().attribute("hair");
+            
+                if (hairs == null) {
+            
+                  hairs = new ArrayList<Hair>();
+            
+                  request.session().attribute("hairs", hairs);
+            
+                }
+            
+    
+                String description = request.queryParams("description");
+            
+                Hair newHair = new Hair(description);
+            
+                hair.add(newHair);
+            
+                model.put("template", "templates/success.vtl");
+            
+                return new ModelAndView(model, layout);
+            
+               }, new VelocityTemplateEngine());
 
     }
 }
