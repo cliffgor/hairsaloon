9adcf57c487f579fab91eff5fa425d94869293ca
cliffgor <clierozigy@gmail.com>
2017-08-31 10:31:20 +0300

    added databaserule.java

---
+++ src/test/HairTest.java/DatabaseRule.java
@@ -0,0 +1,20 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before() {
+      DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_saloon_test", "postgres", "cliero");
+    }
+  
+    @Override
+    protected void after() {
+      try(Connection con = DB.sql2o.open()) {
+        String deleteTasksQuery = "DELETE FROM tasks *;";
+        String deleteCategoriesQuery = "DELETE FROM categories *;";
+        con.createQuery(deleteTasksQuery).executeUpdate();
+        con.createQuery(deleteCategoriesQuery).executeUpdate();
+      }
+    }
+  
+}
